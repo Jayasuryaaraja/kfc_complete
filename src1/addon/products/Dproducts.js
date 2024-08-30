e0f5048a6c7d73fee98b23e3231e9afba0289e4e
Jayasuryaaraja <jayasuryaaraja@gmail.com>
2024-08-30 12:46:37 +0530

    product

---
+++ src1/addon/products/Dproducts.js
@@ -0,0 +1,85 @@
+import A2 from '../images/Dpro8.jpg'
+import A3 from '../images/Dpro7.jpg'
+import A4 from '../images/Dmm.jpg'
+import A5 from '../images/Dpro1.jpg'
+import A6 from '../images/Dpro2.jpg'
+import A7 from '../images/Dpro3.jpg'
+import A8 from '../images/Dpro4.jpg'
+import A9 from '../images/Dpro5.jpg'
+import A10 from '../images/Dpro6.jpg'
+// import A4 from '../images/'
+const Dproducts = [
+    {
+        id: 1,
+        title:"Free 3pc Strips on",
+        image: A2,
+        ds:"Free 3pc Strips on a cart value of 499 or more. Sunday only",
+        price:200,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        title:"Free Chicken zinger",
+        image: A3,
+        ds:"1 Pc free Chicken Zinger on a cart value of 499 or above on first order. Only for",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 3,
+        title:"Upto Rs 100 off on",
+        image: A4,
+        ds:"Upto Rs 100 off on min cart value of Rs 699 or more . Applicable on 4th order",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 4,
+        title:"Free Chicken zinger",
+        image: A5,
+        ds:"1 Pc free Chicken Zinger on a cart value of 499 or above on first order. Only for",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 9,
+        title:"Add 2 Pc Hot ",
+        image: A6,
+        ds:"Add 2 Pc Hot n Crispy Chicken at just Rs 99 on min cart value of Rs 499 or more.",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 10,
+        title:"Upto Rs 100 off on",
+        image: A7,
+        ds:"Upto Rs 100 off on min cart value of Rs 699 or more . Applicable on 4th orde",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 11,
+        title:"Get flat Rs.75 off on",
+        image: A8,
+        ds:"Get flat Rs.75 off on a cart value of Rs.599 or above",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 12,
+        title:"Free Popcorn (Med.",
+        image: A9,
+        ds:"Free Popcorn (Med) on a cart value of 499 or more. Sunday only",
+        quantity: 1,
+        price:200,
+    },
+    {
+        id: 13,
+        title:"Free 3pc Strips on a",
+        image: A10,
+        ds:"Free 3pc Strips on a cart value of 499 or more. Sunday only",
+        quantity: 1,
+        price:200,
+    }
+]
+export default Dproducts;
---
+++ src1/addon/products/Mproduct.js
@@ -0,0 +1,42 @@
+import M1 from '../images/Internburger.jpg'
+import M2 from '../images/Mpro.jpg'
+import M3 from '../images/Mpro1.jpg'
+import M4 from '../images/Mpro2.jpg'
+
+
+
+const Mproduct = [
+    {
+        id: 5,
+        title:"Caribbean Spicy Zinger Burger",
+        image: M1,
+        ds:"New premium zinger with crunchy zinger",
+        quantity: 1,
+        price:219.05,
+    },
+    {
+        id: 6,
+        title:"Korean Tangy Chicken Roll",
+        image: M2,
+        ds:"Delicious roll non tangy Korean chicken strips",
+        quantity: 1,
+        price:308.57,
+    },
+    {
+        id: 7,
+        title:"Korean Tangy Roll & Zinger Chicken Meal",
+        image: M3,
+        ds:"Get 18% off on Korean Chicken Roll,pepsi",
+        quantity: 1,
+        price:388.67,
+    },
+    {
+        id: 8,
+        title:"Thai Spicy Roll & Popcorn Chicken Meal",
+        image: M4,
+        ds:"Get 18% off on Thai Chicken,Med.Popcorn & Pepsi",
+        quantity: 1,
+        price:287.05,
+    }
+]
+export default Mproduct;
